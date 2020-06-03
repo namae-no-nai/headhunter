f4fb1951e9ea3a0955dd0fb6e18b46aa0308ba53
Namae-no-nai <guilhermebluiz@hotmail.com>
2020-06-03 00:21:22 -0300

    aumentando a cobertura dos testes para job_opening

--- spec/features/applicant_applies_to_job_spec.rb
+++ spec/features/applicant_applies_to_job_spec.rb
@@ -23,5 +23,21 @@ feature 'Applicant applies for a job' do
       expect(page).to have_content('pleno')
       expect(page).to have_link('Ver minha candidatura')
     end
+    scenario 'cannot be blank' do
+      user = create(:user)
+      applicant = create(:applicant, user: user)
+      job_vacancy = create(:job_vacancy)
+
+      login_as user, scope: :user
+
+      visit root_path
+      click_on 'Ver vagas de emprego'
+      click_on 'Ver detalhes'
+      click_on 'Me candidatar'
+      fill_in 'Motivação', with: ''
+      click_on 'Enviar'
+
+      expect(page).to have_content('Carta não pode ficar em branco')
+    end
   end
 end
--- spec/features/head_fills_job_vacancy_spec.rb
+++ spec/features/head_fills_job_vacancy_spec.rb
@@ -32,4 +32,51 @@ feature 'Head creates job vacancy' do
     click_on 'Voltar'
     expect(current_path).to eq(root_path)
   end
+  scenario 'cannot be blank' do
+    head = create(:head)
+
+    login_as head, scope: :head
+
+    visit root_path
+    click_on 'Cadastrar vaga de emprego'
+    expect(current_path).to eq(new_job_vacancy_path)
+
+    fill_in 'Vaga', with: ''
+    fill_in 'Descrição da vaga', with: ''
+    fill_in 'Habilidades desejáveis', with: ''
+    select 'R$1000,00 a R$2000,00', from: 'Faixa salarial'
+    select 'pleno', from: 'Nível da vaga'
+    fill_in 'Data limite', with: ''
+    fill_in 'Localização da vaga', with: ''
+
+    click_on 'Enviar'
+
+    expect(page).to have_content('Vaga não pode ficar em branco')
+    expect(page).to have_content('Descrição da vaga não pode ficar em branco')
+    expect(page).to have_content('Habilidades desejáveis não pode ficar em branco')
+    expect(page).to have_content('Data limite não pode ficar em branco')
+    expect(page).to have_content('Localização da vaga não pode ficar em branco')
+  end
+
+  scenario 'date before today' do
+    head = create(:head)
+
+    login_as head, scope: :head
+
+    visit root_path
+    click_on 'Cadastrar vaga de emprego'
+    expect(current_path).to eq(new_job_vacancy_path)
+
+    fill_in 'Vaga', with: 'Desenvolvedor pleno'
+    fill_in 'Descrição da vaga', with: 'uma descrição qualquer de qualquer coisa'
+    fill_in 'Habilidades desejáveis', with: 'dolore magna aliqua. Ut enim ad.'
+    select 'R$1000,00 a R$2000,00', from: 'Faixa salarial'
+    select 'pleno', from: 'Nível da vaga'
+    fill_in 'Data limite', with: 1.day.ago
+    fill_in 'Localização da vaga', with: 'Moema'
+
+    click_on 'Enviar'
+
+    expect(page).to have_content('Data limite não pode ser anterior a hoje') 
+  end
 end
